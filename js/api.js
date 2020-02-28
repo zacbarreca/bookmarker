0e4ae6341234cc4fc6148023616cebd7847971dd
Zac Barreca <zacbarreca@gmail.com>
2020-02-28 12:17:34 -0500

    Update api.js

--- js/api.js
+++ js/api.js
@@ -2,20 +2,18 @@ const apiURL = 'https://thinkful-list-api.herokuapp.com/zacbarreca/bookmarks';
 
 
 // makes a request from the api
-// under construction
 function apiRequest() {
   return fetch(apiURL)
     .then(res => res.json());
 }
 
 // adds a bookmark with a POST request
-// under construction
 function addBookmark(newBookmark) {
-  return fetch(apiURL), {
+  return fetch(apiURL, {
     method: 'POST',
-    headers: { 'content-type': 'application.json' },
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(newBookmark)
-  }
+  })
     .then(res => res.json());
 }
 
