58a478c2565799dd892295d4636a874786bb70c2
Zac Barreca <zacbarreca@gmail.com>
2020-02-28 05:42:16 -0500

    Update store.js

--- js/store.js
+++ js/store.js
@@ -5,7 +5,12 @@ let adding = false;
  * EXPORTS
  **********/
 
+function addBookmark(object) {
+  this.bookmarks.push({ object, expanded: false });
+}
+
 export default {
   bookmarks,
-  adding
+  adding,
+  addBookmark
 };
